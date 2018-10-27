286649d6f62b284b8ed43da597f784c1d971e251
Ranjith_k <rxk180004@utdallas.edu>
2018-10-27 02:50:53 -0500

    updated BubbleSort.java

--- Bubblesort.java
+++ Bubblesort.java
@@ -5,7 +5,7 @@ public class Bubblesort {
 	public static int[] sort(int[] arr){
 		int temp=0;
 		for (int j=0;j<arr.length-1;j++) {
-			for(int i=0;i<arr.length-j-1;i++) {
+			for(int i=2;i<arr.length-j-1;i++) {
 			if(arr[i]>arr[i+1]) {
 				temp=arr[i];
 				arr[i]=arr[i+1];
